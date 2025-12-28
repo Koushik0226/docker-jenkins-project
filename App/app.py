84bed260702f1a022796cfcc2681bc807ecd33e5
Koushik0226 <koushiksomashankar@gmail.com>
2025-12-28 20:39:06 +0530

    Updated homepage to V2

--- App/app.py
+++ App/app.py
@@ -4,13 +4,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    # This looks for index.html inside the 'templates' folder
-    # If you don't have an html file yet, just return "Hello World"
     try:
         return render_template('index.html')
     except:
         return "Hello from your Containerized Python App!"
 
 if __name__ == '__main__':
-    # Host='0.0.0.0' is crucial for Docker containers to be accessible
     app.run(host='0.0.0.0', port=5000)
