9c2403d3998fc320d92c43f9c244fb54a298867a
Koushik0226 <koushiksomashankar@gmail.com>
2026-01-03 18:52:56 +0530

    updated

--- App/app.py
+++ App/app.py
@@ -1,13 +1,14 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    try:
-        return render_template('index.html')
-    except:
-        return "Hello from your Containerized Python App!"
+    pod_name = os.getenv("POD_NAME", "Not Set")
+    pod_ip = os.getenv("POD_IP", "Not Set")
+    
+    return render_template("index.html", pod_name=pod_name, pod_ip=pod_ip, version="v2.0")
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
